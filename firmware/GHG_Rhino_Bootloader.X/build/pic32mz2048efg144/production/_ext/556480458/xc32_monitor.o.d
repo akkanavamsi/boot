dedfb28cf4591a70e03da125105d8821f40de448
vamsi <vamsidevops49@.com>
2022-04-28 20:29:12 +0530

    second

--- firmware/GHG_Rhino_Bootloader.X/build/pic32mz2048efg144/production/_ext/556480458/xc32_monitor.o.d
+++
@@ -1,2 +0,0 @@
-build/pic32mz2048efg144/production/_ext/556480458/xc32_monitor.o: \
- ../src/config/pic32mz2048efg144/stdio/xc32_monitor.c
--- firmware/GHG_Rhino_Bootloader_EFM.X/build/pic32mz2048emf/production/_ext/479398035/xc32_monitor.o.d
+++
@@ -1,2 +0,0 @@
-build/pic32mz2048emf/production/_ext/479398035/xc32_monitor.o: \
- ../src/config/pic32mz2048emf/stdio/xc32_monitor.c
